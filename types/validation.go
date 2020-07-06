8d0337808a7446bce3de3e80c4ff9ac64979fa1f
midenaio <m@idena.io>
2020-07-07 01:30:20 +0500

    Update error messages

--- types/validation.go
+++ types/validation.go
@@ -7,17 +7,17 @@ import (
 
 func (r SubmitTranslationRequest) Validate() error {
 	if r.Word > 3299 {
-		return errors.New("invalid value 'word'")
+		return errors.New("Invalid value 'word'")
 	}
 	if len(r.Name) == 0 || len(r.Name) > 30 {
-		return errors.New("invalid value 'name'")
+		return errors.New("Translation exceeds the maximum length")
 	}
 	if len(r.Description) > 150 {
-		return errors.New("invalid value 'description'")
+		return errors.New("Translation descritption exceeds the maximum length")
 	}
 	var timestamp time.Time
 	if err := timestamp.UnmarshalText([]byte(r.Timestamp)); err != nil {
-		return errors.New("invalid value 'timestamp'")
+		return errors.New("Invalid value 'timestamp'")
 	}
 	return nil
 }
@@ -25,7 +25,7 @@ func (r SubmitTranslationRequest) Validate() error {
 func (r VoteRequest) Validate() error {
 	var timestamp time.Time
 	if err := timestamp.UnmarshalText([]byte(r.Timestamp)); err != nil {
-		return errors.New("invalid value 'timestamp'")
+		return errors.New("Invalid value 'timestamp'")
 	}
 	return nil
 }
